1a4c119b2ff1df96972ba31fb1c5721c10822d98
Jiyoung Kim <jiykim@c8r2s14.42seoul.kr>
2020-10-25 16:16:30 +0900

    add header, main, str_int, handle_error

---
+++ handle_error.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_error.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiykim <jiykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 15:31:57 by jiykim            #+#    #+#             */
+/*   Updated: 2020/10/25 16:12:19 by jiykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		argc_error(int argc)
+{
+	if(argc != 2)
+		return (0);
+	return (1);
+}
+
+int		size_error(int *str)
+{
+	int size;
+
+	size = 0;
+	while(*str)
+	{
+		str++;
+		size++;
+	}
+	if (size != 31)
+		return (0);
+	return (1);
+}
+
+int		layout_error(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 1)
+		{
+			if (str[i] != ' ')
+				return (0);
+		}
+		else
+		{
+			if (str[i] >= '1' && str[i] <= '4')
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int		handle_error(int argc, char **argv)
+{
+	if (argc_error(argc) == 0)
+	{
+		write(1, "argument number error\n", 21);
+		return (0);
+	}
+	if (size_error(argv[1]) == 0)
+	{
+		write(1, "string size error\n", 17);
+		return (0);
+	}
+	if (layout_error(argv[1]) == 0)
+	{
+		write(1, "space error\n", 11);
+		return (0);
+	}
+	return (1);
+}
---
+++ main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiykim <jiykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 14:57:58 by jiykim            #+#    #+#             */
+/*   Updated: 2020/10/25 16:07:33 by jiykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+int		main(int argc, char **argv)
+{
+	int **arr;
+
+	arr = (int **)malloc(sizeof(int) * N);
+	if (handle_error(argc, argv) == 0)
+		return (0);
+	str_int(argv[1], arr);
+}
