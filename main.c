6ab7ee5f2009b6649a667e40f113ded7a8068b47
Jiyoung Kim <jiykim@c8r2s14.42seoul.kr>
2020-10-25 17:53:43 +0900

    mod main 2dimensional malloc

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jiykim <jiykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 14:57:58 by jiykim            #+#    #+#             */
-/*   Updated: 2020/10/25 17:26:45 by jiykim           ###   ########.fr       */
+/*   Updated: 2020/10/25 17:51:01 by jiykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 int		main(int argc, char **argv)
 {
+	int i;
 	int **arr;
 
-	arr = (int **)malloc(sizeof(int *) * N);
 	if (handle_error(argc, argv) == 0)
 		return (0);
+	arr = (int **)malloc(sizeof(int *) * N);
+	i = 0;
+	while (i < N)
+		arr[i++] = (int *)malloc(sizeof(int) * N);
 	str_int(argv[1], arr);
 	free(arr);
 	return (0);
