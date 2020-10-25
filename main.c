8aadc461ef3683c9a406ba8882e034beb43b6430
Jiyoung Kim <jiykim@c8r2s14.42seoul.kr>
2020-10-25 17:28:28 +0900

    mod main malloc space

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jiykim <jiykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 14:57:58 by jiykim            #+#    #+#             */
-/*   Updated: 2020/10/25 17:23:27 by jiykim           ###   ########.fr       */
+/*   Updated: 2020/10/25 17:26:45 by jiykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		main(int argc, char **argv)
 {
 	int **arr;
 
-	arr = (int **) malloc (sizeof(int *) * N);
+	arr = (int **)malloc(sizeof(int *) * N);
 	if (handle_error(argc, argv) == 0)
 		return (0);
 	str_int(argv[1], arr);
